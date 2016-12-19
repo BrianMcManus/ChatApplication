81f390dd0feb1d5e39326963ce1d9278cfb2d2d4
Niall <nmulready@hotmail.com>
2016-12-19 12:24:57 +0000

    MessageDAO and UserDAO implement Interfaces

--- ChatApplication/src/DAO/MessageDAO.java
+++ ChatApplication/src/DAO/MessageDAO.java
@@ -5,10 +5,50 @@
  */
 package DAO;
 
+import DTO.Message;
+import Interfaces.MessageDAOInterface;
+import java.sql.Date;
+import java.util.ArrayList;
+
 /**
  *
  * @author Megatronus
  */
-public class MessageDAO {
+public class MessageDAO implements MessageDAOInterface{
+
+    @Override
+    public ArrayList<Message> getForumMessages() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Message> getUserMessages(int userId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Message getMessageById(int userId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Message> getMessageByDate(Date date) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Message> getUnreadMessages(int userId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean sendMessage(Message message) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void recieveMessage() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
--- ChatApplication/src/DAO/UserDAO.java
+++ ChatApplication/src/DAO/UserDAO.java
@@ -5,10 +5,65 @@
  */
 package DAO;
 
+import DTO.User;
+import Interfaces.UserDAOInterface;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Megatronus
  */
-public class UserDAO {
+public class UserDAO implements UserDAOInterface{
+    private User user;
+
+    @Override
+    public User getUserById(int userId) {Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        user = new User();
+
+        try {
+            con = getConnection();
+            String query = "Select * from classes where classId = ?";
+            ps = con.prepareStatement(query);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                cClass.setName(rs.getString("className"));
+                cClass.setDesc(rs.getString("classDesc"));
+                cClass.setRole(rs.getString("classRole"));
+                cClass.setAlignment(rs.getString("classAlignment"));
+                cClass.setHitDie(rs.getString("classHitDie"));
+                cClass.setSkills(rs.getString("classSkills"));
+                cClass.setFeatures(rs.getString("classFeatures"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(CharacterClassDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }}
+
+    @Override
+    public User login(String userName, String password) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void logout() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean register(User newUser) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean loggedIn(int userId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
