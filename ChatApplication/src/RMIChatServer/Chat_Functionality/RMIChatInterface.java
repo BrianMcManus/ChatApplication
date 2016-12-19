21681601af104d3b3c3e5f7ae1ae14d6eabdafc9
Niall <nmulready@hotmail.com>
2016-12-19 15:50:03 +0000

    RMI Server started

---
+++ ChatApplication/src/RMIChatServer/Chat_Functionality/RMIChatInterface.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RMIChatServer.Chat_Functionality;
+
+import DTO.Message;
+import DTO.User;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ *
+ * @author Megatronus
+ */
+public interface RMIChatInterface extends Remote
+{
+    public boolean addMessage(Message newMessage) throws RemoteException;
+    public Message getMessage() throws RemoteException;
+    
+    public boolean register(User newUser) throws RemoteException;
+    public boolean login(User user) throws RemoteException;
+    
+    public boolean registerForCallback(RMIChatClientInterface client) throws RemoteException;
+    public boolean unregisterForCallback(RMIChatClientInterface client) throws RemoteException;
+}
+
---
+++ ChatApplication/src/RMIChatServer/Launch/RMIServer.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RMIChatServer.Launch;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Megatronus
+ */
+public class RMIServer {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            RMIQuoteImpl quoteObject = new RMIQuoteImpl();
+            
+            int portNum = 55555;
+            startRegistry(portNum);
+            
+            String registryPath = "rmi://localhost:" + portNum;
+            String objectLabel = "/quoteService";
+            
+            Naming.rebind(registryPath+objectLabel, quoteObject);
+        } catch (RemoteException ex) {
+            Logger.getLogger(RMIQuoteServer.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(RMIQuoteServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+        
+    // This method starts a RMI registry on the local host, if it
+    // does not already exist at the specified port number.
+    private static void startRegistry(int RMIPortNum) throws RemoteException 
+    {
+        try {
+            // Try to get the registry at a specific port number
+            // If there is no registry started on that port, an exception will be thrown
+            Registry registry = LocateRegistry.getRegistry(RMIPortNum);
+            
+            registry.list();
+        } catch (RemoteException ex) {
+            // No valid registry at that port.
+            System.out.println("RMI registry cannot be located at port " + RMIPortNum);
+            
+            // Create a registry on the given port number
+            Registry registry = LocateRegistry.createRegistry(RMIPortNum);
+            System.out.println("RMI registry created at port " + RMIPortNum);
+        }
+    } // end startRegistry
+}
